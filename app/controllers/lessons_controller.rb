80220d238e32c90864ee1647260bc5bb7d6cffaf
Kayla Anderson <kaylanicolette@live.com>
2019-04-09 15:47:38 +0000

    Lesson Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_lesson.section.course != current_user.enrolled_in?(@course)
+      redirect_to course_path, alert: 'You are not enrolled'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
