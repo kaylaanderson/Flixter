5e5663d603deb1e4f5252939988020a1407bfc7a
Kayla Anderson <kaylanicolette@live.com>
2019-04-09 23:36:37 +0000

    Ordering Lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,8 +8,8 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_course
-    if current_lesson.section.course != current_user.enrolled_in?(@course)
-      redirect_to course_path, alert: 'You are not enrolled'
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not enrolled'
     end
   end
 
