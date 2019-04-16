0aa10a073d9982cf4bad92d9c2edb7e6e44ecb66
Kayla Anderson <kaylanicolette@live.com>
2019-04-16 01:06:53 +0000

    New Section Form Modal

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,12 @@ Rails.application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :sections, only: [:update]
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do 
-      resources :sections, only: [:new, :create] 
+      resources :sections, only: [:create] 
     end
   end
 end
